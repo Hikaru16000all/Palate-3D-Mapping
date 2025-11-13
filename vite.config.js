e524cad4b3f450023afa46059a456d7ab6e8eb52
Hikaru16000all <2185243657@qq.com>
2025-11-13 13:06:13 +0800

    Change output directory from 'dist' to 'public'

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/Palate-3D-Mapping/', // 重要：替换为你的仓库名
   build: {
-    outDir: 'dist',
+    outDir: 'public',
     sourcemap: false
   }
 })
